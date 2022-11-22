c380c693aec5bc669d2c40ec02e41210ef3f3401
Nikita <405663260nikita@gmail.com>
2022-11-22 11:38:56 +0300

    add documentation to utils.py

--- utils.py
+++ utils.py
@@ -3,14 +3,27 @@ import json
 FILE_NAME = 'candidates.json'
 
 
-def load_candidates():
+def load_candidates() -> list:
+    """
+    Загружает список кандидатов из json-файла
+    Returns: список кандидатов
+
+    """
     with open(FILE_NAME, encoding='utf-8') as file:
         candidates = json.load(file)
 
     return candidates
 
 
-def get_by_pk(pk):
+def get_by_pk(pk: int) -> dict | None:
+    """
+    Позволяет получит информацию о кандидаты по его pk
+    Args:
+        pk: personal key
+
+    Returns: словарь с информацией о кандидате, если он есть в базе
+
+    """
     candidates = load_candidates()
 
     for candidat in candidates:
@@ -18,7 +31,15 @@ def get_by_pk(pk):
             return candidat
 
 
-def get_by_skill(skill_name):
+def get_by_skill(skill_name: str) -> list:
+    """
+    Позволяет получить список кандидатов, обладающих указанным навыком
+    Args:
+        skill_name: навык
+
+    Returns: список с кандидатами
+
+    """
     candidates = load_candidates()
     candidates_with_skill = []
 
